97da50975b2265a386e2d006de1e77f737b75f15
Phonthep Aungkanukulwit <mm@Phontheps-MacBook-Air.local>
2021-08-16 00:25:36 +0700

    api calling, map data, type, casting data, flex

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,10 +1,39 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import classes from "./Home.module.css";
+
+type UserData = {
+  id: String;
+  name: String;
+  lastName: String;
+  nickName: String;
+};
 
 const Home = () => {
+  const [userData, setUserData] = useState<UserData[]>([]);
+
+  useEffect(() => {
+    axios
+      .get("https://sheet.best/api/sheets/4539054f-7c99-4923-b456-9e48297a0ddb")
+      .then((res) => {
+        console.log(res.data);
+
+        setUserData(res.data as UserData[]);
+      })
+      .catch((err) => {});
+  }, []);
+
   return (
     <div>
       <p>Home</p>
+      {userData.map((user, index) => (
+        <div className={classes.UserBox} key={`${user.id}_${user.name}`}>
+          <span>{user.name}</span>
+          <span>{user.lastName}</span>
+          <span>{user.nickName}</span>
+        </div>
+      ))}
       <Link to="/details">go to details</Link>
     </div>
   );
