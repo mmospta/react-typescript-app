aa49a9e9edb70cb6e3b01cc91cc3a61c59faa83d
Phonthep Aungkanukulwit <mm@Phontheps-MacBook-Air.local>
2021-08-15 22:29:01 +0700

    add page router

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Component/Header/Header";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Details from "./Pages/Details/Details";
+import Home from "./Pages/Home/Home";
+import NotFound from "./Pages/NotFound/NotFound";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/details">
+            <Details />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/">
+            <NotFound />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
